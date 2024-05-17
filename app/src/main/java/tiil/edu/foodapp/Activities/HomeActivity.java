c42f5c9bbd58aa9b9170ffe3a5e8a92a18ca5b05
trungtin03 <trungtintran1103@gmail.com>
2024-05-18 01:58:33 +0700

    update

--- app/src/main/java/tiil/edu/foodapp/Activities/HomeActivity.java
+++ app/src/main/java/tiil/edu/foodapp/Activities/HomeActivity.java
@@ -10,14 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 import tiil.edu.foodapp.R;
 
 public class HomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(layout.activity_home);
 
+        LottieAnimationView lottieAnimationView = findViewById(R.id.lottieAnimationView);
+        lottieAnimationView.setAnimation(R.raw.shoping2); // Đặt hoạt ảnh
+        lottieAnimationView.setRepeatCount(LottieDrawable.INFINITE); // Lặp lại vô hạn
+        lottieAnimationView.playAnimation(); // Bắt đầu phát hoạt ảnh
     }
 }
