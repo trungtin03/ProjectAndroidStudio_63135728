2c4dd8dc965d832a2fe5acd7eb29316b63709ace
trungtin03 <trungtintran1103@gmail.com>
2024-05-17 13:45:58 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    //add sdp ssp
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
 }
