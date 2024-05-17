416ad0c7373bc346aeb10a7136155e179996f6b3
trungtin03 <trungtintran1103@gmail.com>
2024-05-17 23:55:31 +0700

    update

---
+++ app/src/main/java/tiil/edu/foodapp/Activities/HomeActivity.java
@@ -0,0 +1,23 @@
+package tiil.edu.foodapp.Activities;
+
+import static tiil.edu.foodapp.R.*;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import tiil.edu.foodapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
--- app/src/main/java/tiil/edu/foodapp/Activities/SplashActivity.java
+++ app/src/main/java/tiil/edu/foodapp/Activities/SplashActivity.java
@@ -1,22 +1,34 @@
 package tiil.edu.foodapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
 import tiil.edu.foodapp.R;
+import tiil.edu.foodapp.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding biding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-
+        biding = DataBindingUtil.setContentView(this,R.layout.activity_splash);
+        biding.tvgetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
